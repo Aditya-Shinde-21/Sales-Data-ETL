f465491448117cf71888d5d7a150e9b7fd4fb3aa
Aditya-Shinde-21 <139449660+Aditya-Shinde-21@users.noreply.github.com>
2025-12-26 22:29:19 +0530

    Update dimension_tables_join.py

--- scripts/main/transformations/jobs/dimension_tables_join.py
+++ scripts/main/transformations/jobs/dimension_tables_join.py
@@ -44,40 +44,5 @@ def dimension_table_joins(sales_df,customer_df,store_df,sales_team_df,product_df
             sales_team_df.address.alias("sales_person_address"),
             sales_team_df.pincode.alias("sales_person_pincode")
         )
-    '''
-    #step 1 where I am joining customer table
-    #But I do not need all the columns so dropping columns which are not needed
-    #save the result into s3_customer_df_join
-    logger.info("Joining the final_df_to_process with customer_table_df ")
-    s3_customer_df_join = final_df_to_process.alias("s3_data") \
-        .join(customer_table_df.alias("ct"),
-              col("s3_data.customer_id") == col("ct.customer_id"),"inner") \
-        .drop("product_id","price","quantity","additional_column",
-              "s3_data.customer_id","customer_joining_date")
-
-    final_df_to_process.unpersist()
-    #s3_customer_df_join.printSchema()
-
-    #step 2 where I am joining store table details
-    #dropping columns which are not needed
-    #save the result into s3_customer_store_df_join
-    logger.info("Joining the s3_customer_df_join with store_table_df ")
-    s3_customer_store_df_join= s3_customer_df_join.join(store_table_df,
-                                store_table_df["id"]==s3_customer_df_join["store_id"], "inner")\
-                                .drop("id","store_pincode","store_opening_date")
-
-    #step 3 where I am adding sales team table details
-    #dropping columns which are not needed
-    #save the result into s3_customer_store_sales_df_join
-    logger.info("Joining the s3_customer_store_df_join with sales_team_table_df ")
-    s3_customer_store_sales_df_join = s3_customer_store_df_join.join(sales_team_table_df.alias("st"),
-                             col("st.id")==s3_customer_store_df_join["sales_person_id"], "inner")\
-                .withColumn("sales_person_first_name",col("st.first_name"))\
-                .withColumn("sales_person_last_name",col("st.last_name"))\
-                .withColumn("sales_person_address",col("st.address"))\
-                .withColumn("sales_person_pincode",col("st.pincode")) \
-                .withColumn("sales_date_year", year(col("sales_date"))) \
-                .withColumn("sales_date_month", month(col("sales_date"))) \
-                .drop("id","st.first_name","st.last_name","st.address","st.pincode")
-    '''
+    
     return result_df
