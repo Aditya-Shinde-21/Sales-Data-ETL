b402c1aabc22d34a4f2d77d793c7767bbf023436
Aditya-Shinde-21 <139449660+Aditya-Shinde-21@users.noreply.github.com>
2025-12-26 22:28:16 +0530

    Update customer_mart_sql_transform_write.py

--- scripts/main/transformations/jobs/customer_mart_sql_transform_write.py
+++ scripts/main/transformations/jobs/customer_mart_sql_transform_write.py
@@ -8,7 +8,6 @@ from scripts.main.write.database_write import DatabaseWriter
 #calculation for customer mart
 #find out the customer total purchase every month
 #write the data into MySQL table
-
 def customer_mart_calculation_table_write(final_customer_data_mart_df):
 
     result_df = final_customer_data_mart_df\
@@ -30,29 +29,11 @@ def customer_mart_calculation_table_write(final_customer_data_mart_df):
             col("sales_date_month"),
             col("total_sales"))
 
-    '''
-    window = Window.partitionBy(col("customer_id"), col("sales_date_year"), col("sales_date_month"))
 
-    final_customer_data_mart = final_customer_data_mart_df\
-                    .withColumn("sales_date_year", year(col("sales_date")))\
-                    .withColumn("sales_date_month", month(col("sales_date")))\
-                    .withColumn("total_sales_every_month_by_each_customer",
-                                sum(col("total_cost")).over(window))\
-                    .select(col("customer_id"),
-                            concat(col("first_name"),lit(" "),col("last_name")).alias("full_name"),
-                            col("address"),
-                            col("phone_number"),
-                            col("sales_date_year"),
-                            col("sales_date_month"),
-                            col("total_sales_every_month_by_each_customer").alias("total_sales"))\
-                    .distinct()
-
-    final_customer_data_mart.show()
-    '''
     result_df.persist(StorageLevel.MEMORY_AND_DISK)
     result_df.show()
-
     #Write the Data into MySQL customers_data_mart table
     db_writer = DatabaseWriter(url=config.url,properties=config.properties)
     db_writer.write_dataframe(result_df,config.customer_data_mart_table)
+
     result_df.unpersist(blocking=True)
